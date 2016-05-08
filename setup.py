292734818627db27d1f4158a35513a40ece11c41
Parkayun <iamparkayun@gmail.com>
2016-05-08 18:59:57 +0900

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+import re
+from setuptools import setup
+
+
+with open('sofabase/__init__.py') as f:
+    version = re.search(r'__version__\s*=\s*\'(.+?)\'', f.read()).group(1)
+assert version
+
+setup(
+    name="sofabase",
+    version=version,
+    packages=["sofabase"],
+    install_requires=["couchbase>=2.0.7"],
+    author="Ayun Park",
+    author_email="iamparkayun@gmail.com",
+    description="Couchbase Object Mapper",
+    url="http://github.com/Parkayun/sofabase",
+)
