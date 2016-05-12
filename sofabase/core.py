23838c28fc6ced273c15e01d23a4b25de61e4d45
Parkayun <iamparkayun@gmail.com>
2016-05-12 10:27:00 +0900

    Fix validate logic

--- sofabase/core.py
+++ sofabase/core.py
@@ -15,9 +15,10 @@ class Model(object):
     def get_document(self):
         document = {}
         for name, field in self.__fields__.items():
-            if field.is_key or not field.validate():
-                continue
-            document[name] = field.value
+            if field.validate():
+                if field.is_key:
+                    continue
+                document[name] = field.value
         return document
 
     def setup_fields(self, fields):
